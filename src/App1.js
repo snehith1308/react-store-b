f0208e6dae7abd83bad25aabdf8850a5a52f3aeb
snehith1308 <ambewarsnehith138@gmail.com>
2025-03-13 01:16:13 +0530

    modified new file

---
+++ src/App1.js
@@ -0,0 +1,7 @@
+import React from 'react'
+export default function App1()
+{
+    return(
+        <div style={{backgroundColor:'yellow'}}>App1</div>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css"
+export default function Header()
+{
+    return(
+        <div>
+            <div className="App-Header-Row">
+            <div>React Store</div>
+            <div>Menu</div>
+            </div>
+        </div>
+    )
+}
